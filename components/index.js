24d9cabb2a34290fd70b8fd2b23857bdaf4b9b10
Nexthood <sztoika.corp@proton.me>
2025-12-31 16:49:45 +0100

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,10 @@
+// dataset components
+export { default as Sidebar } from "./dataset/Sidebar";
+export { default as Navbar } from "./dataset/Navbar";
+export { default as KpiCard } from "./dataset/KpiCard";
+export { default as CompareChart } from "./dataset/CompareChart";
+export { default as AutoChart } from "./dataset/AutoChart";
+export { default as ChartSuggestions } from "./dataset/ChartSuggestions";
+export { default as CorrelationHeatmap } from "./dataset/CorrelationHeatmap";
+export { default as DatasetPreview } from "./dataset/DatasetPreview";
+export { default as SchemaInspector } from "./dataset/SchemaInspector";
